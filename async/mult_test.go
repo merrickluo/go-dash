92c067a28733f980f39b3aa134d921e95533e2ff
ahxxm <i@ahxxm.com>
2021-09-17 13:13:35 +0800

    fix test mult

--- async/mult_test.go
+++ async/mult_test.go
@@ -1,30 +1,29 @@
-package async_test
+package async
 
 import (
 	"testing"
 
-	"github.com/merrickluo/go-dash/async"
 	"github.com/stretchr/testify/assert"
 )
 
-func testMultiply(t *testing.T) {
+func TestMultiply(t *testing.T) {
 	g := make(chan int)
 	ch1 := make(chan int, 10)
 	ch2 := make(chan int, 10)
 
-	m := async.Mult(g)
-	async.Tap(m, ch1)
-	async.Tap(m, ch2)
+	m := Mult(g)
+	Tap(m, ch1)
+	Tap(m, ch2)
 
 	g <- 10
 
-	async.Untap(m, ch1)
+	Untap(m, ch1)
 
 	g <- 20
 	close(g)
 	close(ch1)
 	close(ch2)
 
-	assert.Equal(t, []int{10}, async.Collect(ch1))
-	assert.Equal(t, []int{10, 20}, async.Collect(ch2))
+	assert.Equal(t, []int{10}, Collect(ch1))
+	assert.Equal(t, []int{10, 20}, Collect(ch2))
 }
